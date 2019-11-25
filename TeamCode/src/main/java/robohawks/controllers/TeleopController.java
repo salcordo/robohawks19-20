0f9fae5475dfb0877f9071b90c06e23d8cec53ba
Sean <salcordo@hendricken.com>
2019-11-25 17:39:17 -0500

    Removed last years codes + added in precision movement and lifting and lowering the arm.

--- TeamCode/src/main/java/robohawks/controllers/TeleopController.java
+++ TeamCode/src/main/java/robohawks/controllers/TeleopController.java
@@ -1,44 +1,27 @@
-package robohawks.controllers;
+        package robohawks.controllers;
 
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.Range;
+        import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+        import com.qualcomm.robotcore.hardware.CRServo;
+        import com.qualcomm.robotcore.hardware.DcMotor;
+        import com.qualcomm.robotcore.hardware.Servo;
+        import com.qualcomm.robotcore.util.Range;
 
-import robohawks.controllers.old.Controller;
-import robohawks.modules.base.HolonomicDriveModule;
+        import robohawks.controllers.old.Controller;
+        import robohawks.modules.base.HolonomicDriveModule;
 
 @TeleOp(name = "TeleOp", group = "Teleop")
 public class TeleopController extends Controller {
 
     HolonomicDriveModule drive;
-    DcMotor liftArm;
-    DcMotor liftArm2;
-    DcMotor mineralArmm;
-    DcMotor mineralArms;
-    //DcMotor mineralSpool;
-    Servo phone;
-    Servo drop;
-    CRServo mineralCollector;
+    DcMotor motorLift;
 
-    int tog = -1;
+    float precisionx = 1;
+    float precisiony = 1;
 
     @Override
     public void init() {
         drive = new HolonomicDriveModule(hardwareMap);
-        liftArm = hardwareMap.dcMotor.get("liftArm");
-        liftArm2 = hardwareMap.dcMotor.get("liftArm2");
-        mineralArmm = hardwareMap.dcMotor.get("mineralArmm");
-        mineralArms = hardwareMap.dcMotor.get("mineralArms");
-        //mineralSpool = hardwareMap.dcMotor.get("mineralSpool");
-        phone = hardwareMap.servo.get("phone");
-        drop = hardwareMap.servo.get("drop");
-        mineralCollector = hardwareMap.crservo.get("pipe");
-
-        phone.setPosition(1);
-
-        telemetry.addData("mineralCollector", mineralCollector.getPower());
+        motorLift = hardwareMap.dcMotor.get("motorLift");
     }
 
     @Override
@@ -46,107 +29,50 @@ public class TeleopController extends Controller {
         super.loop();
 
         // DRIVE
-        float gamepad1LeftY = gamepad2.left_stick_y;
-        float gamepad1LeftX = -gamepad2.left_stick_x;
-        float gamepad1RightX = gamepad2.right_stick_x * (tog * -1);
+        float gamepad1LeftY = gamepad1.left_stick_y * precisiony;
+        float gamepad1LeftX = -gamepad1.left_stick_x * precisionx;
+        float gamepad1RightX = gamepad1.right_stick_x * precisionx;
 
-                // holonomic formulas
+        // holonomic formulas
         float FrontLeft = -gamepad1LeftY - gamepad1LeftX - gamepad1RightX;
         float FrontRight = gamepad1LeftY - gamepad1LeftX - gamepad1RightX;
         float BackRight = gamepad1LeftY + gamepad1LeftX - gamepad1RightX;
         float BackLeft = -gamepad1LeftY + gamepad1LeftX - gamepad1RightX;
 
-                // clip the right/left values so that the values never exceed +/- 1
+        // clip the right/left values so that the values never exceed +/- 1
         FrontRight = Range.clip(FrontRight, -1, 1);
         FrontLeft = Range.clip(FrontLeft, -1, 1);
         BackLeft = Range.clip(BackLeft, -1, 1);
         BackRight = Range.clip(BackRight, -1, 1);
 
-                // write the values to the motors
-        drive.setPowerOne(-FrontRight/2 * tog);
-        drive.setPowerTwo(FrontLeft/2 * tog);
-        drive.setPowerThree(BackLeft/2 * tog);
-        drive.setPowerFour(-BackRight/2 * tog);
+        // write the values to the motors
+        drive.setPowerOne(-FrontRight/2);
+        drive.setPowerTwo(FrontLeft/2);
+        drive.setPowerThree(BackLeft/2);
+        drive.setPowerFour(-BackRight/2);
 
-        //TOGGLE
-        if (gamepad2.dpad_up){
-            tog = -1;
-        } else if (gamepad2.dpad_down){
-            tog = 1;
-        }
 
-        // LIFT ARM
+        //PRECISION X AND Y
         if (gamepad1.dpad_down){
-            liftArm.setPower(1);
-            liftArm2.setPower(-1);
+            precisionx = (float) 0.6;
+            precisiony = (float) 0.4;
+        } else if (gamepad1.dpad_up){
+            precisionx = (float) 1;
+            precisiony = (float) 1;
+        }
+
+        //LIFT ARM
+        if(gamepad2.right_bumper){
+            motorLift.setPower(-1);
         } else {
-            liftArm.setPower(0);
-            liftArm2.setPower(0);
+            motorLift.setPower(0);
         }
 
-        if (gamepad1.dpad_up){  
-            liftArm.setPower(-1);
-            liftArm2.setPower(1);
+        //LOWER ARM
+        if(gamepad2.left_bumper){
+            motorLift.setPower(1);
         } else {
-            liftArm.setPower(0);
-            liftArm2.setPower(0);
-        }
-
-        // MINERAL ARM
-        if (gamepad1.x){
-            mineralArmm.setPower(-.85);
-            mineralArms.setPower(-.85);
-        } else {
-            mineralArmm.setPower(0);
-            mineralArms.setPower(0);
-        }
-
-        if (gamepad1.a){
-            mineralArmm.setPower(.85);
-            mineralArms.setPower(.85);
-        } else {
-            mineralArmm.setPower(0);
-            mineralArms.setPower(0);
-        }
-
-//        // MINERAL SPOOL
-//        if (gamepad1.b){
-//            mineralSpool.setPower(1);
-//        } else {
-//            mineralSpool.setPower(0);
-//        }
-//
-//        if (gamepad1.y){
-//            mineralSpool.setPower(-1);
-//        } else {
-//            mineralSpool.setPower(0);
-//        }
-
-        // MINERAL COLLECTION
-        if (gamepad1.right_bumper){
-            mineralCollector.setPower(1);
-        }
-
-        if (gamepad1.left_bumper) {
-            mineralCollector.setPower(-1);
-        }
-
-        if (gamepad1.dpad_left){
-            mineralCollector.setPower(0);
-        }
-
-        // PHONE
-        if (gamepad1.right_stick_button){
-            phone.setPosition(.1);
-        }else{
-            phone.setPosition(0);
-        }
-
-        // DROP
-        if (gamepad1.left_stick_button){
-            drop.setPosition(1);
-        }else{
-            drop.setPosition(0);
+            motorLift.setPower(0);
         }
     }
 }
