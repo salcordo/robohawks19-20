9b9b933242253166d29e222f772b547a14d4767f
Sean <salcordo@hendricken.com>
2019-12-12 16:44:10 -0500

    All code is tested and works. Toggle is fixed.
    TODO:
    Compliance wheels
    Autonomous

--- TeamCode/src/main/java/robohawks/controllers/TeleopController.java
+++ TeamCode/src/main/java/robohawks/controllers/TeleopController.java
@@ -18,12 +18,18 @@ public class TeleopController extends Controller {
     Servo servoLoaf;
     Servo servoFoundation;
 
-
     float precisionxa = 1;
     float precisionya = 1;
     float precisionxb = 1;
     float precisionyb = 1;
     int armposition;
+    boolean precisionreset = true;
+    boolean precisiontoggle = false;
+    boolean loafreset = true;
+    boolean loaftoggle = false;
+    boolean foundationreset = true;
+    boolean foundationtoggle = false;
+
 
     @Override
     public void init() {
@@ -37,6 +43,7 @@ public class TeleopController extends Controller {
     @Override
     public void loop() {
         super.loop();
+
 //CONTROLLER 1
     //Left Stick:   Drive
     //Right Stick:  Rotate
@@ -44,15 +51,16 @@ public class TeleopController extends Controller {
     //DPAD:         Motor Test
 
 //CONTROLLER 2
-    //Left Bumper:  Curl
-    //Right Bumper: Curl
+    //Left Bumper:  Curl Up
+    //Right Bumper: Curl Down
     //X:            Loaf
     //Y:            Foundation
+    //B:            Bridge Reset
 
         // Drive
         float gamepad1LeftY = gamepad1.left_stick_y * precisionya;
         float gamepad1LeftX = -gamepad1.left_stick_x * precisionxa;
-        float gamepad1RightX = -gamepad1.right_stick_x * precisionxa;
+        float gamepad1RightX = gamepad1.right_stick_x * precisionxa;
 
         // Holonomic Formulas
         float FrontLeft = -gamepad1LeftY - gamepad1LeftX - gamepad1RightX;
@@ -67,10 +75,10 @@ public class TeleopController extends Controller {
         BackRight = Range.clip(BackRight, -1, 1);
 
         // Write the values to the motors
-        drive.setPowerOne(-FrontRight/2);
-        drive.setPowerTwo(FrontLeft/2);
-        drive.setPowerThree(BackLeft/2);
-        drive.setPowerFour(-BackRight/2);
+        drive.setPowerOne(-FrontRight);
+        drive.setPowerTwo(FrontLeft);
+        drive.setPowerThree(BackLeft);
+        drive.setPowerFour(-BackRight);
 
 
         //EXAMPLE BUTTON
@@ -84,20 +92,23 @@ public class TeleopController extends Controller {
         //  servo.setPosition(0);
 
         //Toggle precision mode for primary driver (Controller 1)
-        boolean precision = false;
-        if(gamepad1.b && !precision){
-            if(precisionxa == 1) {
-                precisionya = (float) 0.4;
-                precisionxa = (float) 0.6;
-            } else {
-                precisionxa = (float) 1;
-                precisionya = (float) 1;
-                precision = true;
-            }
-        } else if (!gamepad1.b){
-            precision = false;
+        if(gamepad1.b && precisionreset){
+            precisiontoggle = !precisiontoggle;
+            precisionreset = false;
         }
 
+        if(!gamepad1.b){
+            precisionreset = true;
+        }
+
+        if(precisiontoggle){
+            precisionya = (float) 0.4;
+            precisionxa = (float) 0.6;
+        }
+        if(!precisiontoggle){
+            precisionya = (float) 1;
+            precisionxa = (float) 1;
+        }
         //Curl
         if(gamepad2.left_bumper){
             motorLCurl.setPower(1);
@@ -109,53 +120,75 @@ public class TeleopController extends Controller {
 
         //Curl
         if(gamepad2.right_bumper){
-            motorLCurl.setPower(-1);
-            motorRCurl.setPower(-1);
+            motorLCurl.setPower(-0.5);
+            motorRCurl.setPower(-0.5);
         }else{
             motorLCurl.setPower(0);
             motorRCurl.setPower(0);
         }
 
-        //Servo Loaf
-        boolean loaf = false; //Outside of loop()
-        if(gamepad2.x && !loaf) {
-            if(servoLoaf.getPosition() == 0) servoLoaf.setPosition(90);
-            else servoLoaf.setPosition(0);
-            loaf = true;
-        } else if(!gamepad2.x) loaf = false;
-
         //Servo Foundation
-        boolean foundation = false; //Outside of loop()
-        if(gamepad2.y && !foundation) {
-            if(servoFoundation.getPosition() == 0) servoFoundation.setPosition(90);
-            else servoFoundation.setPosition(0);
-            foundation = true;
-        } else if(!gamepad2.x) foundation = false;
+        if(gamepad2.y && foundationreset){
+            foundationtoggle = !foundationtoggle;
+            foundationreset = false;
+        }
+
+        if(!gamepad2.y){
+            foundationreset = true;
+        }
+
+        if(foundationtoggle){
+            servoFoundation.setPosition(0);
+        }
+        if(!foundationtoggle){
+            servoFoundation.setPosition(90);
+        }
+        //Servo Loaf
+        if(gamepad2.x && loafreset){
+            loaftoggle = !loaftoggle;
+            loafreset = false;
+        }
+
+        if(!gamepad2.x){
+            loafreset = true;
+        }
+
+        if(loaftoggle){
+            servoLoaf.setPosition(0);
+        }
+        if(!loaftoggle){
+            servoLoaf.setPosition(90);
+        }
+
+        //BRIDGE RESET
+        if(gamepad2.b){
+            loaftoggle = false;
+            foundationtoggle = false;
+        }
 
         //DPAD TESTING
         if(gamepad1.dpad_up){
-            drive.setPowerOne(0.5);
+            drive.setPowerOne(1);
         } else {
             drive.setPowerOne(0);
         }
 
         if(gamepad1.dpad_left){
-            drive.setPowerTwo(0.5);
+            drive.setPowerTwo(1);
         } else {
             drive.setPowerTwo(0);
         }
 
         if(gamepad1.dpad_right){
-            drive.setPowerThree(0.5);
+            drive.setPowerThree(1);
         } else {
             drive.setPowerThree(0);
         }
 
         if(gamepad1.dpad_down){
-            drive.setPowerFour(0.5);
+            drive.setPowerFour(1);
         } else {
             drive.setPowerFour(0);
         }
     }
-
 }
